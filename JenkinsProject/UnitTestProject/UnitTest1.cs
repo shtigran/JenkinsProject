48a3d03b6839fd33e6a3164066c43607d2a56330
Tiko <Tiko@TIKO->
2019-11-10 16:38:28 +0400

    Added Unit Test

---
+++ JenkinsProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathFunctions;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Functions.sum(3, 4), 7);
+        }
+    }
+}
