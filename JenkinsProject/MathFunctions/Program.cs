d5abd9d72c23ecd68f104b056152982b1571e58d
Tiko <Tiko@TIKO->
2019-11-12 00:54:26 +0400

    Correct the last error

--- JenkinsProject/MathFunctions/Program.cs
+++ JenkinsProject/MathFunctions/Program.cs
@@ -11,7 +11,7 @@ namespace MathFunctions
         static void Main(string[] args)
         {
             Console.WriteLine("The sum is: " + Functions.sum(7, 8));
-            asd
+
             Console.ReadKey();
         }
     }
