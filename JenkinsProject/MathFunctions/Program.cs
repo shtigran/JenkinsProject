77eb11b30bd979365fc301b5198621d04d61655a
Tiko <Tiko@TIKO->
2019-11-12 22:19:27 +0400

    Added NewtonSoft from Nuget

--- JenkinsProject/MathFunctions/Program.cs
+++ JenkinsProject/MathFunctions/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace MathFunctions
         static void Main(string[] args)
         {
             Console.WriteLine("The sum is: " + Functions.sum(7, 8));
+
+            var json = JsonConvert.SerializeObject("Hello");
 
             Console.ReadKey();
         }
