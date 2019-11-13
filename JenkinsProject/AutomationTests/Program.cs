2478e1a1e5828de27665db4ff7ebb73e52b98e8f
Tiko <Tiko@TIKO->
2019-11-13 22:09:26 +0400

    Added Automation test

---
+++ JenkinsProject/AutomationTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new WebException() {};
+        }
+    }
+}
